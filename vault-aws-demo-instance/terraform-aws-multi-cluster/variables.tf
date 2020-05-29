3ecb82b104784537ff175f7e42595f2047fd06a2
Jim <ray.jim@gmail.com>
2020-05-29 14:10:05 -0400

    initial commit

---
+++ vault-aws-demo-instance/terraform-aws-multi-cluster/variables.tf
@@ -0,0 +1,34 @@
+# AWS region in which to deploy
+variable aws_region {}
+
+# All resources will be tagged with this
+variable environment_name {}
+
+variable consul_dc {}
+
+variable vault_server_count {
+  default = 1
+}
+
+# URL for Vault OSS binary
+variable vault_zip_file {
+  default = "https://releases.hashicorp.com/vault/1.4.0/vault_1.4.0_linux_amd64.zip"
+}
+
+# URL for Consul OSS binary
+variable consul_zip_file {
+  default = "https://releases.hashicorp.com/consul/1.7.0/consul_1.7.0_linux_amd64.zip"
+}
+
+# Instance size
+variable instance_type {}
+
+# SSH key name to access EC2 instances (should already exist)
+variable key_name {}
+
+# Instance tags for HashiBot AWS resource reaper
+variable hashibot_reaper_owner {}
+
+variable hashibot_reaper_ttl {
+  default = 48
+}

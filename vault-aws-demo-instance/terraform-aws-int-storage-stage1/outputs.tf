798b5c122555b5594905d6823dc1eb667c56ab4f
Jim <ray.jim@gmail.com>
2020-08-16 16:01:59 -0400

    remove uneeded dir

--- vault-aws-demo-instance/terraform-aws-int-storage-stage1/outputs.tf
+++ vault-aws-demo-instance/terraform-aws-int-storage-stage1/outputs.tf
@@ -1,19 +1,3 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
-
-output "subnet_ids" {
-  value = aws_subnet.main.*.id
-}
-
-output "security_group_id" {
-  value = aws_security_group.main.id
-}
-
-output "kms_id" {
-  value = aws_kms_key.s3.key_id
-}
-
-output "bucket_name" {
-  value = var.bucket_name
-}
--- vault-aws-demo-instance/terraform-aws-int-storage-stage1/variables.tf
+++ vault-aws-demo-instance/terraform-aws-int-storage-stage1/variables.tf
@@ -1,21 +1,8 @@
 variable "aws_region" {
   description = "AWS region"
+  default = "us-east-1"
 }
 
 variable "namespace" {
   description = "Unique name to use for DNS and resource naming"
-}
-
-variable "bucket_name" {
-  description = "Name of the PTFE source bucket to create"
-}
-
-variable "cidr_block" {
-  description = "CIDR block to use for VPC"
-  default     = "10.0.0.0/16"
-}
-
-variable "subnet_count" {
-  description = "number of subnets to create"
-  default     = "2"
 }

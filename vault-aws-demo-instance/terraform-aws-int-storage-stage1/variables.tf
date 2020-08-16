d8cbcd5f4fbcdcb5d36b3f97514d986ab623efe1
Jim <ray.jim@gmail.com>
2020-08-16 16:09:05 -0400

    updated stage 1 code
    builds VPC only

--- vault-aws-demo-instance/terraform-aws-int-storage-stage1/variables.tf
+++ vault-aws-demo-instance/terraform-aws-int-storage-stage1/variables.tf
@@ -1,8 +1,13 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-east-1"
+  default     = ""
 }
 
 variable "namespace" {
   description = "Unique name to use for DNS and resource naming"
+}
+
+variable "cidr_block" {
+  description = "base CIDR for VPC"
+  default     = ""
 }

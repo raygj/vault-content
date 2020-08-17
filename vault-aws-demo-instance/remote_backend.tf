78e9f1dede75f371c2ce553e709e4541d8731be5
Jim <ray.jim@gmail.com>
2020-08-17 17:31:56 -0400

    attempt to clean up workspace

--- vault-aws-demo-instance/remote_backend.tf
+++ vault-aws-demo-instance/remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "jray-hashi"
     workspaces {
       name = "aws-us-east-1-vault-int-storage-stage2"

1fd5f390590d9fd79ee70114aba27f807cee09cd
Jim <ray.jim@gmail.com>
2020-08-17 17:13:44 -0400

    updated main
    trying to destroy mis-named resources

---
+++ vault-aws-demo-instance/terraform-aws-int-storage-stage2/remote_backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "jray-hashi"
+    workspaces {
+      name = "aws-us-east-1-vault-int-storage-stage2"
+    }
+  }
+}

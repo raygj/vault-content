295125c4083f7e3f40513a922743adb98aa2877b
Jim <ray.jim@gmail.com>
2020-08-16 21:22:45 -0400

    Update main.tf
    try hard coded VPC ID

--- vault-aws-demo-instance/terraform-aws-int-storage-stage2/main.tf
+++ vault-aws-demo-instance/terraform-aws-int-storage-stage2/main.tf
@@ -6,7 +6,7 @@ module "vault-starter" {
   source                = "app.terraform.io/jray-hashi/vault-starter/aws"
   version               = "0.1.2"
   allowed_inbound_cidrs = ["var.allowed_inbound_cidrs"]
-  vpc_id                = "var.input_vpc_id"
+  vpc_id                = "vpc-9cd6c8e6"
   vault_version         = "var.vault_version"
   owner                 = "var.owner"
   name_prefix           = "var.namespace"

a9437a31492435ab74b1db7b87f83775ee238bb4
Jim <ray.jim@gmail.com>
2020-08-17 20:36:36 -0400

    Update main.tf

--- vault-aws-demo-instance/terraform-aws-int-storage-stage2/main.tf
+++ vault-aws-demo-instance/terraform-aws-int-storage-stage2/main.tf
@@ -6,7 +6,7 @@ module "vault-oss" {
   source                = "hashicorp/vault-oss/aws"
   version               = "0.2.0"
   allowed_inbound_cidrs = ["100.14.98.25/32"]
-  vpc_id                = "vpc-9cd6c8e6"
+  vpc_id                = "vpc-2790344c"
   vault_version         = "1.5.0"
   owner                 = "jray-at-hashicorp"
   name_prefix           = "jray"
